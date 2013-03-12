4c073bab8b8aef56e334c305c0d77a85df49633c
Travis J Parker <travis.parker@gmail.com>
2013-03-12 11:28:45 -0700

    tests for the new option header parsing
    Change-Id: I049d04d968115641e62f3bc4a31b2706cd5da677

---
+++ test/unit/test_parsing.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# vim: fileencoding=utf8:et:sta:ai:sw=4:ts=4:sts=4
+
+import unittest
+
+import pathfinder
+
+
+class HeaderParsingTests(unittest.TestCase):
+    def test_option_header_urlencoded_ctype_no_charset(self):
+        self.assertEqual(
+                pathfinder._parse_option_header(
+                    "application/x-www-form-urlencoded"),
+                ('application/x-www-form-urlencoded', {}))
+        self.assertEqual(
+                pathfinder._parse_option_header(
+                    "application/x-form-url-encoded"),
+                ('application/x-form-url-encoded', {}))
+
+    def test_option_header_urlencoded_ctype_with_charset(self):
+        self.assertEqual(
+                pathfinder._parse_option_header(
+                    "application/x-www-form-urlencoded; charset=UTF8"),
+                ('application/x-www-form-urlencoded', {'charset': 'UTF8'}))
+        self.assertEqual(
+                pathfinder._parse_option_header(
+                    "application/x-form-url-encoded; charset=UTF8"),
+                ('application/x-form-url-encoded', {'charset': 'UTF8'}))
+
+
+if __name__ == '__main__':
+    unittest.main()
